5e50929f5247af1e875d25fdbe54a10a1d671cfc
Staaz1990 <alex-carole@outlook.fr>
2019-01-13 11:46:28 +0100

    class

---
+++ script.js
@@ -0,0 +1,155 @@
+$('.Carré2').click(function() {
+  $('.Carré2, .Rond5, .Rond6, .Rond7, .Rond8').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Carré1').click(function() {
+  $('.Carré1, .Rond1, .Rond2, .Rond3, .Rond4').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Losange').click(function() {
+  $('.Rond2, .Rond6, .Rond9, .Rond10, .Rond11, .Rond12, .Rond15, .Rond19, .Losange').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Carré3').click(function() {
+  $('.Rond13, .Rond14, .Rond15, .Rond16, .Carré3').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Carré4').click(function() {
+  $('.Rond17, .Rond18, .Rond19, .Rond20, .Carré4').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond1').click(function() {
+  $('.Rond1').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond2').click(function() {
+  $('.Rond2').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond3').click(function() {
+  $('.Rond3').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond4').click(function() {
+  $('.Rond4').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond5').click(function() {
+  $('.Rond5').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond6').click(function() {
+  $('.Rond6').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond7').click(function() {
+  $('.Rond7').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond8').click(function() {
+  $('.Rond8').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond9').click(function() {
+  $('.Rond9').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond10').click(function() {
+  $('.Rond10').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond11').click(function() {
+  $('.Rond11').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond12').click(function() {
+  $('.Rond12').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond13').click(function() {
+  $('.Rond13').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond14').click(function() {
+  $('.Rond14').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond15').click(function() {
+  $('.Rond15').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond16').click(function() {
+  $('.Rond16').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond17').click(function() {
+  $('.Rond17').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond18').click(function() {
+  $('.Rond18').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond19').click(function() {
+  $('.Rond19').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.Rond20').click(function() {
+  $('.Rond20').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
+
+$('.All').click(function() {
+  $('.Carré1, .Carré2, .Carré3, .Carré4, .rond, .Losange').each(function() {
+  	$(this).toggleClass("active");
+  });
+});
